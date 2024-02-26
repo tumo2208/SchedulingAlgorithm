f54b4e4bfc0fe1ac93705f92f841d53a68ec2144
tumo2208 <tuadc1234@gmail.com>
2024-02-26 12:09:28 +0700

    Create prj

---
+++ src/main/java/com/example/schedulingalgorithm/AppStart.java
@@ -0,0 +1,16 @@
+package com.example.schedulingalgorithm;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppStart extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
